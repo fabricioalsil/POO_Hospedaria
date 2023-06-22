a23a93687987aad4787c2e9020e546af79c37e7f
isabelamenditti <87388235+isabelamenditti@users.noreply.github.com>
2023-06-22 15:34:59 -0300

    Merge branch 'main' of https://github.com/fabricioalsil/POO_Hospedaria into main

--- POO_Final/src/models/Hospedagem.java
+++ POO_Final/src/models/Hospedagem.java
@@ -134,6 +134,7 @@ public class Hospedagem implements Serializable{
 			listarPgtos.append(pgto.getTipo()).append(" R$ ");
 			listarPgtos.append(pgto.getValor()).append("\n");
 		}
+		
 		listarPgtos.append("> SUBTOTAL: R$" + totalPagamentos() + "\n");
 				
 		return listarPgtos;
