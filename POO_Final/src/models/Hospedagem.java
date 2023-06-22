d1aa15a9e4ffcba714e8a48bf3491886adaeee82
isabelamenditti <87388235+isabelamenditti@users.noreply.github.com>
2023-06-22 15:39:07 -0300

    Ajustes Hospedagem exibição de extrato

--- POO_Final/src/models/Hospedagem.java
+++ POO_Final/src/models/Hospedagem.java
@@ -2,7 +2,7 @@ package models;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.UUID;
 
 import enums.ETipoPagamento;
@@ -20,8 +20,8 @@ public class Hospedagem implements Serializable{
 	
 	//variaveis
 	private final String id;
-	private final Date dataCheckin;
-	private Date dataCheckout = null;
+	private final Calendar dataCheckin;
+	private Calendar dataCheckout = null;
 	private IHospede hospede;
 	private ArrayList<IHospede> acompanhantes = new ArrayList<>();
 	private IAcomodacao quarto;
@@ -30,21 +30,25 @@ public class Hospedagem implements Serializable{
 	
 	public Hospedagem(IHospede hospede, IAcomodacao quarto) {
 		this.id = UUID.randomUUID().toString();
-		this.dataCheckin = new Date();
+		this.dataCheckin = Calendar.getInstance();
 		this.hospede = hospede;
 		this.quarto = quarto;
 		this.conta = new Conta();
 	}
 	
-	public Date getDataCheckin() {
+	public String getId() {
+		return id;
+	}
+		
+	public Calendar getDataCheckin() {
 		return dataCheckin;
 	}
 
-	public Date getDataCheckout() {
+	public Calendar getDataCheckout() {
 		return dataCheckout;
 	}
 
-	public void setDataCheckout(Date dataCheckout) {
+	public void setDataCheckout(Calendar dataCheckout) {
 		this.dataCheckout = dataCheckout;
 	}
 
@@ -73,52 +77,95 @@ public class Hospedagem implements Serializable{
 	}
 
 	public void addPagamento(ETipoPagamento tipo, double valor) {
+		if(tipo == ETipoPagamento.ESTORNO) {
+			valor = -valor;
+		}
 		pagamento.add(new Pagamento(tipo, valor));
 	}
 	
-	public void addEstorno(double valor) {
-		pagamento.add(new Pagamento(ETipoPagamento.ESTORNO, -valor));
+	public int numDiarias() {
+		int numDiarias = 0;
+		
+		Calendar dataHoje = Calendar.getInstance();
+
+		int hora = dataHoje.get(Calendar.HOUR_OF_DAY);
+		int dia = dataHoje.get(Calendar.DAY_OF_YEAR);
+		
+		numDiarias = dia - getDataCheckin().get(Calendar.DAY_OF_YEAR);
+		
+		if(hora > getLimitecheckout()) {
+			++numDiarias;
+		}
+				
+		return numDiarias;
+	}
+		
+	public double valorTotalDiarias() {
+		double total = 0;
+				
+		total = numDiarias()*(getQuarto().getTarifaDiaria() + getQuarto().getAdicionalAcompanhante()*acompanhantes.size());
+				
+		return total;
 	}
 	
-	public String getExtrato() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Gastos: \n");
-		sb.append(conta.listar());
-		sb.append("Pagamentos feitos: \n");
+	public double totalPagamentos() {
+		double total = 0;
 		
-		double valorFinal = 0;
-		
-		for (Pagamento pago : pagamento) {
-			sb.append(pago.getTipo() + " R$ " + pago.getValor() + "\n");
-			valorFinal += pago.getValor();
-		}
-		sb.append("Pagamentos totais: R$" + valorFinal + "\n\n");
-		sb.append("Valor final: \n");
-		valorFinal -= conta.getTotal();
-		
-		if(valorFinal > 0) {
-			sb.append("Credito de R$ " + valorFinal + "\n");
-		}else if(valorFinal < 0) {
-			valorFinal *= -1;
-			sb.append("Debito de R$ " + valorFinal + "\n");
-		}else {
-			sb.append("Não há pendencias\n");
+		for(Pagamento pgto: pagamento) {
+			total += pgto.getValor();
 		}
 		
-		return sb.toString();
+		return total;
 	}
 	
-	public String getId() {
-		return id;
+	public double saldoDevedor() {
+		double saldoDevedor = 0;
+		
+		saldoDevedor = valorTotalDiarias() + conta.getTotal() - totalPagamentos();
+		
+		return saldoDevedor;
+	}
+	
+	public StringBuilder listarPagamentos() {
+		StringBuilder listarPgtos = new StringBuilder();
+		
+		for(Pagamento pgto : pagamento){
+			listarPgtos.append("Data: ").append(pgto.getData()).append("\t");
+			listarPgtos.append(pgto.getTipo()).append(" R$ ");
+			listarPgtos.append(pgto.getValor()).append("\n");
+		}
+		
+		listarPgtos.append("> SUBTOTAL: R$" + totalPagamentos() + "\n");
+				
+		return listarPgtos;
+	}
+	
+	public String extrato() {
+		StringBuilder extrato = new StringBuilder();
+		
+		extrato.append("DIARIAS: \n");
+		extrato.append("Quantidade: " + numDiarias() + "\n");
+		extrato.append("Preço: R$" + getQuarto().getTarifaDiaria() + "\n");
+		extrato.append("Adicional Acompanhante: R$" + getQuarto().getAdicionalAcompanhante() + "\n");
+		extrato.append("> SUBTOTAL: R$" + valorTotalDiarias() + "\n\n");
+		
+		extrato.append("DESPESAS DE HOSPEDAGEM: \n");
+		extrato.append(getConta().listar()).append("\n");
+		
+		extrato.append("PAGAMENTOS REALIZADOS: \n");
+		extrato.append(listarPagamentos()).append("\n");
+		
+		extrato.append(">>>>> TOTAL = R$" + saldoDevedor() + "\n");
+				
+		return extrato.toString();
 	}
 	
 	//Checkout
 	public void checkout() {
-		
+		setDataCheckout(Calendar.getInstance());
 	}
 
 	//Métodos da classe
-
 	public static int getIniciocheckin() {
 		return INICIO_CHECKIN;
 	}
